5ffd13279360b334319d185e8a2d07934d2aae64
Stefan Hancar <stefan@hancar.sk>
2026-09-09 09:41:32 +0200

    add update

--- app/page.tsx
+++ app/page.tsx
@@ -109,6 +109,7 @@ export default function Home() {
                 alt="Plavecká škola FEDDY"
                 fill
                 priority
+                sizes="(min-width: 1024px) 50vw, 100vw"
                 className="object-cover"
               />
             </div>
